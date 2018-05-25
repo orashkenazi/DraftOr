d7749e92782b9acd792bee951f58acd6f008ba16
Or Ashkenazi <ashkenazior@gmail.com>
2018-05-25 12:36:42 +0400

    fixed whitespace

--- js/app.js
+++ js/app.js
@@ -415,8 +415,8 @@ function changeObjectData(){
 }
 
 function testFunc(x,y){
+    console.log(document.documentElement.clientHeight)
    
-   updateLabels();
 
 }
 
@@ -464,10 +464,8 @@ function loadingOBJObject(path,name){
              //move to mathias zero point! : 55.45933228, -20.875329,  =~ -49,75
             newObject.position.x=-49;
             newObject.position.y=75;
-            myObjects.push(newObject);
-            unselectableObjects.push(newObject);
+            myObjects.push(newObject)
             setObjectsInSelectList(myObjects)
-           
             
            
             
