bff84e44502f21b7cc73d409b558b1c2e5326875
Or Ashkenazi <ashkenazior@gmail.com>
2018-10-11 09:32:59 +0400

    new loading

--- js/app.js
+++ js/app.js
@@ -27,8 +27,8 @@ let mountain;
 
 
 
-// var renderer = new THREE.WebGLRenderer();
-// renderer.domElement.id = 'renderer';
+var renderer = new THREE.WebGLRenderer();
+renderer.domElement.id = 'renderer';
 
 renderer.setSize( window.innerWidth, window.innerHeight);
 renderer.setPixelRatio( window.devicePixelRatio );
@@ -36,6 +36,13 @@ renderer.setPixelRatio( window.devicePixelRatio );
 
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+
+//cameraa setup
+
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 100000000000 );
+
+camera.up = new THREE.Vector3(0,0,1);
+
 
 //defining renderer for interactivepages..
 let interactivePageRenderer = new THREE.WebGLRenderer();
@@ -68,6 +75,151 @@ let selectedObjectIndex;                  //the current selected object in the l
 let myMovie = new Movie(); //create the movie object
 
 
+
+
+function getTerrainPixelData(){
+    var img = document.getElementById("landscape-image");
+   
+    var canvas = document.getElementById("canvas1");
+    
+    canvas.width = img.width;
+    canvas.height = img.height;
+    canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+  
+    var data = canvas.getContext('2d').getImageData(0,0, img.height, img.width).data;
+    var normPixels = []
+  
+    for (var i = 0, n = data.length; i < n; i += 4) {
+      // get the average value of R, G and B.
+      normPixels.push((data[i] + data[i+1] + data[i+2]) / 3);
+    }
+  
+    
+    return normPixels;
+  }
+   
+  
+  function getSeaLevelData(){
+      var img = document.getElementById("sealevel-image");
+     
+      var canvas = document.getElementById("canvas2");
+      
+      canvas.width = img.width;
+      canvas.height = img.height;
+      canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+    
+      var data = canvas.getContext('2d').getImageData(0,0, img.height, img.width).data;
+      var normPixels = []
+    
+      for (var i = 0, n = data.length; i < n; i += 4) {
+        // get the average value of R, G and B.
+        normPixels.push((data[i] + data[i+1] + data[i+2]) / 3);
+      }
+      
+      
+      return normPixels;
+    }
+  
+  
+  
+  
+  function addGround() {
+      //go to zero point => -29.84[km] in X, +7.21[km] in Y
+      /*imgdata??
+      originial pixels = 2592
+      
+      upper width (x) = 74.83km
+      lower dith (x) = 74.47km
+      height (Y) = 80.06km
+  
+      
+      coordinates: (earth coordiantes)
+      minX: 55.18
+      maxX   55.9
+  
+      mmaxY: -20.82
+      minY: -21.54
+  
+      pixelsize: 0.000277778 (each pixel in the picture, eqcual this coordinate distance)
+  
+  
+      */
+      
+  
+      var numSegments = 299;
+    
+      var geometry = new THREE.PlaneGeometry(74830, 80060, numSegments, numSegments); 
+    
+        var material = new THREE.MeshBasicMaterial({color: 0xffffff})
+      
+        
+  
+  
+  
+      terrain = getTerrainPixelData();  
+      sealevel = getSeaLevelData();
+      
+  
+      // keep in mind, that the plane has more vertices than segments. If there's one segment, there's two vertices, if
+      // there's 10 segments, there's 11 vertices, and so forth. 
+      // The simplest is, if like here you have 100 segments, the image to have 101 pixels. You don't have to worry about
+      // "skewing the landscape" then..
+    
+      // to check uncomment the next line, numbers should be equal
+      console.log("length: " + terrain.length + ", vertices length: " + geometry.vertices.length);
+  
+  
+     
+  
+      for (var i = 0, l = geometry.vertices.length; i < l; i++)
+      {   
+          var terrainValue = terrain[i] / 255;
+          if (terrainValue > 2/255) {                 // we let only height>2 pass... in order to keep it flat in first ~ 30meters.
+              geometry.vertices[i].z = geometry.vertices[i].z + (terrainValue *3036) *1.5 ;
+          }
+         
+          
+          if (sealevel[i] == 0){
+             // geometry.vertices[i].z = -2000;
+          }
+        
+          
+           
+      }
+      
+     
+  
+   
+     
+     
+    
+      geometry.computeFaceNormals();
+      geometry.computeVertexNormals();
+    
+      var plane = new THREE.Mesh(geometry,material);
+      //var plane2 = new THREE.Mesh(geometry,material2)
+      plane.position = new THREE.Vector3(0,0,0);
+   
+      var q = new THREE.Quaternion();
+      q.setFromAxisAngle( new THREE.Vector3(0,0,0), 90 * Math.PI / 180 );
+      plane.quaternion.multiplyQuaternions( q, plane.quaternion );
+  
+      plane.name="terrain";
+     
+      //plane.position.x=+74830/2 -29840;
+      //plane.position.y=-80060/2 +7210;
+      plane.position.z=-15;
+      
+      console.log('terrain created');
+      terrainMesh = plane;
+         
+    
+
+  
+     
+  
+      
+    }
 
 
 
@@ -1007,12 +1159,23 @@ function onLoadBody(){
 
     if(!(navigator.userAgent.indexOf('Chrome') > -1)){ //alret if no chrome!
         console.log('its not chrome browser.')
-        let alertdiv = document.getElementById("loading");
-        alertdiv.style.display = 'inline-flex';
+        let alertdiv = document.createElement("div");
+     
+        alertdiv.id = 'alertdiv';
         alertdiv.style.alignItems = 'center';
         alertdiv.style.marginTop = '0px';
+        alertdiv.style.position = 'absolute';
+        alertdiv.style.zIndex = '1005';
+        alertdiv.style.top = '40%';
+        alertdiv.style.left = '30%';
+        alertdiv.style.background = 'rgb(0,0,0,0.8)';
+        alertdiv.style.width = '40%';
+        alertdiv.style.padding = '30px';
         
-        alertdiv.innerHTML = '<div class="sd-content"><p style="font-size:18px; color:red">Warning: browser is not Google Chrome. </p><p>This website is currently under development, and is intended to be viewed only by Google Chrome.</p></div>'
+        
+        
+        alertdiv.innerHTML = "<div class='sd-content'><p style='font-size:18px; color:red; font-weight:bold;'>Attention, le navigateur n'est pas Google Chrome: </p><p style='color:white;'>Le siteweb est en cours de developpement et fonctionne uniquement avec Google Chrome.</p></div>";
+        document.body.appendChild(alertdiv)
     }
 
     
@@ -1357,11 +1520,6 @@ function render() {
   
     
 
-    
-
-   
-   
-
 	if ( (intersects.length > 0) && ( (unselectableObjects.indexOf(intersects[0].object)==-1 ) && (unselectableObjects.indexOf(intersects[0].object.parent)==-1 ) ) ) {
          /*the next "if" statment is because we load objects with groups (childrens..) hence, we need to change the intersectred 
     object to be the group and not the objects. but we dont want to do it to all objects (only the one who are children of objects).
@@ -1400,16 +1558,12 @@ function animate() {
     requestAnimationFrame( animate );
     
     controls.update();
-    if(loadingFinished){
+    
         render();
         updateLabels();
         updatePositionMark(controls.target)
-    }
     
-    else {
-       
-        renderer.render(loadingScene,camera);
-    }
+    
  
     
     
@@ -2053,7 +2207,7 @@ function topView(){
     if (document.getElementById("topView").checked){
         console.log('topview')
         camera.position.x= 0;
-        camera.position.y= 0;
+        camera.position.y= -10;
         camera.position.z= 900;
         setRotation()
 
